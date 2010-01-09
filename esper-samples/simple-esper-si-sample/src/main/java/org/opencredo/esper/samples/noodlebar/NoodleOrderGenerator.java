8d792f2aaac50a931ac8de270fbb3fe81a92e65b
Russ Miles <russ@russmiles.com>
2010-01-09 11:11:58 +0000

    Refined the sample application a little and added a note concerning Esper configuration (using XML) support

--- esper-samples/simple-esper-si-sample/src/main/java/org/opencredo/esper/samples/noodlebar/NoodleOrderGenerator.java
+++ esper-samples/simple-esper-si-sample/src/main/java/org/opencredo/esper/samples/noodlebar/NoodleOrderGenerator.java
@@ -20,49 +20,68 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class NoodleOrderGenerator {
-	
+
 	private static final int NUMBER_OF_ORDERS = 1000;
-	
+
 	private static final String NOODLE_BAR_BEAN_NAME = "noodleBar";
-	
+
 	private static final String NOODLE_ORDER_THROUGHPUT_MONITOR_BEAN_NAME = "noodleOrderThroughputMonitor";
-	
+
 	private static NoodleBar noodleBar;
-	
+
 	private static NoodleOrderThroughputMonitor noodleOrderThroughputMonitor;
 
 	public static void main(String[] args) {
-		
+
 		initializeDependencies();
-		
+
+		long startTime = System.currentTimeMillis();
+
 		sendSomeOrders();
+
+		long stopTime = System.currentTimeMillis();
+
+		long timeTaken = stopTime - startTime;
+
+		System.out.println("Simple timer calculated at end " + timeTaken
+				+ " milliseconds for " + NUMBER_OF_ORDERS + " orders");
+
+		System.out
+				.println("The Noodle Bar is actually processing "
+						+ noodleOrderThroughputMonitor.getAverageThroughput()
+						+ " orders per second according to continuous Esper Monitoring");
 		
-		System.out.println("The Noodle Bar is processing " + noodleOrderThroughputMonitor.getAverageThroughput() + " orders per minute");
+		// Required to force the close of threads opened by Esper
+		System.exit(0);
 	}
 
 	private static void sendSomeOrders() {
 		NoodleOrder[] orders = new NoodleOrder[NUMBER_OF_ORDERS];
-		
+
 		// Create and send events
 		for (int x = 0; x < NUMBER_OF_ORDERS; x++) {
 			orders[x] = new NoodleOrder();
 			noodleBar.placeOrder(orders[x]);
 		}
-		
+
 		for (NoodleOrder order : orders) {
 			if (order.getStatus() == OrderStatus.COMPLETE) {
 				System.out.println("Order Completed!");
 			} else {
-				System.out.println("Order: " + order + " status is " + order.getStatus());
+				System.out.println("Order: " + order + " status is "
+						+ order.getStatus());
 			}
 		}
 	}
 
 	private static void initializeDependencies() {
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:noodlebar-context.xml");
-		
-		noodleBar = (NoodleBar) applicationContext.getBean(NOODLE_BAR_BEAN_NAME);
-		
-		noodleOrderThroughputMonitor = (NoodleOrderThroughputMonitor) applicationContext.getBean(NOODLE_ORDER_THROUGHPUT_MONITOR_BEAN_NAME);
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"classpath:noodlebar-context.xml");
+
+		noodleBar = (NoodleBar) applicationContext
+				.getBean(NOODLE_BAR_BEAN_NAME);
+
+		noodleOrderThroughputMonitor = (NoodleOrderThroughputMonitor) applicationContext
+				.getBean(NOODLE_ORDER_THROUGHPUT_MONITOR_BEAN_NAME);
 	}
 }
--- esper-template/src/main/java/org/opencredo/esper/EsperTemplate.java
+++ esper-template/src/main/java/org/opencredo/esper/EsperTemplate.java
@@ -29,100 +29,113 @@ import com.espertech.esper.client.EPServiceProviderManager;
 import com.espertech.esper.client.EPStatement;
 
 /**
- * The main workhorse of Esper. The template is configured
- * with a set of statements that query the flow of events.
- * Each statement is then associated with a number of listeners
- * who will be notified when there is a result from the statement.
+ * The main workhorse of Esper. The template is configured with a set of
+ * statements that query the flow of events. Each statement is then associated
+ * with a number of listeners who will be notified when there is a result from
+ * the statement.
  * 
- * Once setup the template is then used to inform esper of any 
- * events of interest by calling sendEvent(Object).
+ * Once setup the template is then used to inform esper of any events of
+ * interest by calling sendEvent(Object).
  * 
  * @author Russ Miles (russell.miles@opencredo.com)
- *
+ * 
+ *         TODO Add support for passing a configuration document, embedded and
+ *         resource loaded, to configure the generated EPRuntime
  */
-public final class EsperTemplate implements BeanNameAware, InitializingBean, DisposableBean {
-	
-    private EPServiceProvider epServiceProvider;
-    private EPRuntime epRuntime;
-    private String name;
-    private Set<EsperStatement> statements = new LinkedHashSet<EsperStatement>();
+public final class EsperTemplate implements BeanNameAware, InitializingBean,
+		DisposableBean {
 
-    /**
-     * Add a collection of {@link EsperStatement} to the template.
-     * 
-     * @param statementBeans
-     */
-    public void setStatements(Set<EsperStatement> statements) {
-    	this.statements = statements;
-    }
-    
-    public Set<EsperStatement> getStatements() {
+	private EPServiceProvider epServiceProvider;
+	private EPRuntime epRuntime;
+	private String name;
+	private Set<EsperStatement> statements = new LinkedHashSet<EsperStatement>();
+
+	/**
+	 * Add a collection of {@link EsperStatement} to the template.
+	 * 
+	 * @param statementBeans
+	 */
+	public void setStatements(Set<EsperStatement> statements) {
+		this.statements = statements;
+	}
+
+	public Set<EsperStatement> getStatements() {
 		return this.statements;
-    }
+	}
 
-    /**
-     * Adds an {@link EsperStatement} composite to the template.
-     * 
-     * @param statement The EsperStatement to add to the template. 
-     */
-    public void addStatement(EsperStatement statement) {
-    	statements.add(statement);
-    }
+	/**
+	 * Adds an {@link EsperStatement} composite to the template.
+	 * 
+	 * @param statement
+	 *            The EsperStatement to add to the template.
+	 */
+	public void addStatement(EsperStatement statement) {
+		statements.add(statement);
+	}
 
-    /**
-     * Instructs the template to send an event to Esper.
-     * Events are then used to satisfy statements, which then
-     * alert listeners.
-     * 
-     * @param event The event that Esper is to be informed of.
-     */
-    public void sendEvent(Object event) {
-    	epRuntime.sendEvent(event);
-    }
+	/**
+	 * Instructs the template to send an event to Esper. Events are then used to
+	 * satisfy statements, which then alert listeners.
+	 * 
+	 * @param event
+	 *            The event that Esper is to be informed of.
+	 */
+	public void sendEvent(Object event) {
+		epRuntime.sendEvent(event);
+	}
 
-    /* (non-Javadoc)
-     * @see org.springframework.beans.factory.BeanNameAware#setBeanName(java.lang.String)
-     */
-    public void setBeanName(String name) {
-    	this.name = name;
-    }
-    
-    public String getName() {
-    	return this.name;
-    }
-    
-    /**
-     * Initializes the Esper service provider with the 
-     * provided statements and associated listeners.
-     * 
-     * The provider is giving a unique name that is based on
-     * the bean name.
-     */
-    private void setupEsper() {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.springframework.beans.factory.BeanNameAware#setBeanName(java.lang
+	 * .String)
+	 */
+	public void setBeanName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * Initializes the Esper service provider with the provided statements and
+	 * associated listeners.
+	 * 
+	 * The provider is giving a unique name that is based on the bean name.
+	 */
+	private void setupEsper() {
 		epServiceProvider = EPServiceProviderManager.getProvider(name);
 		epRuntime = epServiceProvider.getEPRuntime();
 		for (EsperStatement statementBean : statements) {
-		    EPStatement epStatement = epServiceProvider.getEPAdministrator().createEPL(statementBean.getEPL());
-		    statementBean.setEPStatement(epStatement);
+			EPStatement epStatement = epServiceProvider.getEPAdministrator()
+					.createEPL(statementBean.getEPL());
+			statementBean.setEPStatement(epStatement);
 		}
-    }
-    
-    /**
-     * Tidies up the Esper service provider, which in turn
-     * releases any resources being used by Esper.
-     */
-    private void cleanup() {
-    	epServiceProvider.destroy();
-    }
+	}
 
-	/* (non-Javadoc)
-	 * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+	/**
+	 * Tidies up the Esper service provider, which in turn releases any
+	 * resources being used by Esper.
+	 */
+	private void cleanup() {
+		epServiceProvider.destroy();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
 	 */
 	public void afterPropertiesSet() throws Exception {
 		this.setupEsper();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.springframework.beans.factory.DisposableBean#destroy()
 	 */
 	public void destroy() throws Exception {
